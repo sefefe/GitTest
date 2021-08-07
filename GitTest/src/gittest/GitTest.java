38bca12f40eb70cd3c37c8c9c0664f45a5f65cc6
sefefe <sefefe@gmail.com>
2021-08-08 01:35:36 +0300

    Update GitTest.java

--- GitTest/src/gittest/GitTest.java
+++ GitTest/src/gittest/GitTest.java
@@ -15,6 +15,8 @@ public class GitTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("hello");
         // TODO code application logic here
     }
     
